434fd9690c11516a7c50bd1114db918c6db1df36
ClementMathew <clementmathew924@gmail.com>
2023-05-14 03:01:25 +0530

    new

--- lib/Home/notification.dart
+++ lib/Home/notification.dart
@@ -15,18 +15,19 @@ class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        toolbarHeight: height * .080,
-        title: const Text(
-          "Notifications",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 20, ),
+        appBar: AppBar(
+          elevation: 0,
+          toolbarHeight: height * .080,
+          title: const Text(
+            "Notifications",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: theme,
         ),
-        centerTitle: true,
-        backgroundColor: theme,
-      ),
-      body:notify()
-    );
+        body: SingleChildScrollView(child: notify()));
   }
 }
--- lib/HomeReusable/notificationContainer.dart
+++ lib/HomeReusable/notificationContainer.dart
@@ -1,19 +1,23 @@
-import 'package:blood_donation/Welcome/WelcomePage.dart';
 import 'package:flutter/material.dart';
 
 import '../Color/praanaColor.dart';
 
 Widget notify() {
   return Container(
-    margin: const EdgeInsets.fromLTRB(15,15, 15, 0),
-    padding: const EdgeInsets.all(10),
-    height: height * .2,
+    margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+    padding: const EdgeInsets.all(18),
+    height: 100,
     width: double.infinity,
-    decoration: BoxDecoration(color: Colors.white,
+    decoration: BoxDecoration(
+        color: Colors.white,
         borderRadius: BorderRadius.circular(20),
-        boxShadow: const [
+        boxShadow: [
           BoxShadow(
-              color: Colors.grey, offset: Offset(5, 5), blurRadius: 10)
+            color: Colors.grey.withOpacity(0.5), // shadow color
+            spreadRadius: 2, // how spread the shadow should be
+            blurRadius: 3, // how blurred the shadow should be
+            offset: const Offset(0, 0), // offset of the shadow
+          ),
         ]),
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -26,11 +30,20 @@ Widget notify() {
               fontWeight: FontWeight.bold,
               letterSpacing: .5),
         ),
-        const Text(
-          "22-05-2023",
-          style: TextStyle(color: Colors.grey, fontSize: 15),
+        const SizedBox(
+          height: 4,
         ),
-        const Text("hi"),
+        const Text(
+          "22-05-2023 , 12:30 pm",
+          style: TextStyle(color: Colors.grey, fontSize: 13),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          "Now you can donate blood .. !",
+          style: TextStyle(fontFamily: "IbarraRealNova", fontSize: 19),
+        ),
       ],
     ),
   );
